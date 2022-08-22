a5de057201d8a4f0064ae24749c4dd0669b50df4
th2 <th2@users.noreply.github.com>
2022-08-22 17:35:56 +0200

    Add some style

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')()
+const express = require('express')
+const app = express()
 const path = require('path')
 const fs = require('fs')
 const users = require('./data/users.json')
 
-express.get('/', (req, res) => res.sendFile('chart.html', {root: path.join(__dirname)}))
-express.get('/users', (req, res) => res.send(users))
-express.get('/checkins/*', (req, res) => res.send(getCheckins(req.params[0])))
-express.get('/data', (req, res) => res.send(getData()))
-express.listen(8888)
+app.get('/', (req, res) => res.sendFile('public/chart.html', {root: path.join(__dirname)}))
+app.get('/users', (req, res) => res.send(users))
+app.get('/checkins/*', (req, res) => res.send(getCheckins(req.params[0])))
+app.get('/data', (req, res) => res.send(getData()))
+app.use('/', express.static('public'))
+app.listen(8888)
 
 function getCheckins(username) {
     return 'todo'
