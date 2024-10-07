3b65488901b6ddb3622a62068cbe2adfd1c84ab3
Thorben Hehmann <th2@users.noreply.github.com>
2024-10-07 22:10:25 +0200

    Remove logger

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cloudflare = require('cloudflare-express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
@@ -7,25 +6,16 @@ const users = require('./data/users.json')
 const updaterRSS = require('./updater-rss.js')
 const updaterDist = require('./updater-distance.js')
 const badges = require('./badges.js')
-const logger = require('./logger')
 
-app.use(cloudflare.restore({update_on_start:true}))
-app.use(logger.visit())
 app.get('/', (req, res) => res.sendFile('public/chart.html', {root: path.join(__dirname)}))
 app.get('/users', (req, res) => res.send(users))
-app.get('/checkins/*', (req, res) => res.send(getCheckins(req.params[0])))
 app.get('/data', (req, res) => res.send(getData()))
 app.get('/dataDistance', (req, res) => res.sendFile('data/distance.json', {root: path.join(__dirname)}, (err) => {if (err) res.send('[]') }))
 app.get('/update', (req, res) => updaterRSS.run(users, () => updaterDist.run(users, () => res.redirect(('/')))))
 app.get('/lastupdate', (req, res) => res.sendFile('data/lastupdate.json', {root: path.join(__dirname)}, (err) => {if (err) res.send('1970-01-01T00:00:00Z') }))
 app.get('/badges', (req, res) => res.send(badges.get(users)))
 app.use('/', express.static('public'))
-app.use(logger.error())
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
-
-function getCheckins(username) {
-    return 'todo'
-}
 
 function getData() {
     return users.map(user => {
