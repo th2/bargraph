cdbaf426c66ce948e47c756b3bcba5b58e2908c0
Thorben Hehmann <th2@users.noreply.github.com>
2024-10-08 19:25:57 +0200

    Refactored badges.js

--- badges.js
+++ badges.js
@@ -1,18 +1,19 @@
-const { all } = require("axios");
 const fs = require("fs");
 
 const DEFAULT_IMAGE = "https://assets.untappd.com/badges/bdg_default_lg.jpg";
 
 module.exports.get = (users) => {
-  let userInfo = [];
-  let allBadges = [];
+  let badges = {
+    list: [],
+    users: [],
+  };
 
   users.forEach((user) => {
     const userBadges = JSON.parse(
       fs.readFileSync(`data/badges-${user.username}.json`, "utf8")
     );
 
-    userInfo.push({
+    badges.users.push({
       name: user.username,
       uniquebadges: userBadges.length,
     });
@@ -20,56 +21,51 @@ module.exports.get = (users) => {
     userBadges
       .filter((badge) => badge.image != DEFAULT_IMAGE)
       .forEach((badge) => {
-        const name = badge.name.split(" (Level ")[0];
-        const level = badge.name.includes(" (Level ")
+        badge.level = badge.name.includes(" (Level ")
           ? badge.name.split(" (Level ")[1].replace(")", "")
           : undefined;
-        const isLevel = badge.name.includes(" (Level ");
-        const date = badge.date ? new Date(badge.date + " UTC") : undefined;
+        badge.isLevel = badge.name.includes(" (Level ");
+        badge.date = badge.date ? new Date(badge.date + " UTC") : undefined;
+        badge.name = badge.name.split(" (Level ")[0];
 
-        const found = allBadges.find((b) => b.name === name);
-        if (found) {
-          found.isLevel = found.isLevel || isLevel;
-          if (found.firstDate > date) {
-            found.firstDate = date;
+        const badgeInList = badges.list.find((b) => b.name === badge.name);
+        if (badgeInList) {
+          badgeInList.isLevel = badgeInList.isLevel || badge.isLevel;
+          if (badgeInList.firstDate > badge.date) {
+            badgeInList.firstDate = badge.date;
           }
-          found.users.push({
-            name: user.username,
-            date: date,
-            level: level,
-          });
+          badgeInList.users.push(makeBadgeUser(badge, user));
         } else {
-          allBadges.push({
-            name: name,
-            image: badge.image,
-            isVenuebadge: badge.isVenuebadge,
-            isRetired: badge.isRetired,
-            isLevel: isLevel,
-            firstDate: date,
-            users: [
-              {
-                name: user.username,
-                date: date,
-                level: level,
-              },
-            ],
-          });
+          badge.firstDate = badge.date;
+          badge.users = [makeBadgeUser(badge, user)];
+          badges.list.push(badge);
         }
       });
   });
 
-  allBadges.sort((a, b) => b.firstDate - a.firstDate);
-  return (
-    '<html><head><title>Badges</title><link rel="stylesheet" href="style.css"></head><body>' +
+  badges.list.sort((a, b) => b.firstDate - a.firstDate);
+
+  return createTable(badges);
+};
+
+const makeBadgeUser = (badge, user) => {
+  return {
+    name: user.username,
+    date: badge.date,
+    level: badge.level,
+  };
+};
+
+const createTable = (badges) => {
+    return '<html><head><title>Badges</title><link rel="stylesheet" href="style.css"></head><body>' +
     "<table><thead><tr><th>Badge</th>" +
-    userInfo
+    badges.users
       .map((user) => "<th>" + user.name + " (" + user.uniquebadges + ")</th>")
       .join("") +
     "</tr></thead><tbody>" +
-    allBadges.map((badge) => createRow(badge, users)).join("") +
-    "</tbody></table></body></html>"
-  );
-};
+    badges.list.map((badge) => createRow(badge, badges.users)).join("") +
+    "</tbody></table></body></html>";
+}
 
 const createRow = (badge, users) => {
   return (
@@ -81,7 +77,7 @@ const createRow = (badge, users) => {
     "</td>" +
     users
       .map((user) => {
-        const userBadge = badge.users.find((b) => b.name === user.username);
+        const userBadge = badge.users.find((b) => b.name === user.name);
         if (userBadge) {
           return (
             "<td>" +
