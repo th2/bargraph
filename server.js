665073b3b7703b6a60862a1fe3ac99950f594fbb
Thorben Hehmann <th2@users.noreply.github.com>
2024-10-09 23:46:51 +0200

    Automatically detect updaters

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
 app.get("/badges", (req, res) => res.send(require("./badges.js").get(users)));
 app.get("/update", (req, res) => update(0, () => res.redirect("/")));
 app.use("/", express.static("public"));
 
-const updaters = ["users.js", "rss.js", "checkins.js", "distance.js"];
+const updaters = fs.readdirSync('./updater');
 const update = (i, callback) => require('./updater/' + updaters[i])
   .run(() => i >= updaters.length - 1 ? callback() : update(i + 1, callback));
