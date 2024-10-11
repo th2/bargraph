3e7a9d7084115ecf9fc2bf0d743d9373877ad6a5
Thorben Hehmann <th2@users.noreply.github.com>
2024-10-11 09:25:37 +0200

    Refactored updaters loading in server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const fs = require('fs');
-const updaters = fs.readdirSync('./updater');
+const updaters = fs.readdirSync('./updater').map(updater => require(`./updater/${updater}`));
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
 app.get("/update", (req, res) => update(0, () => res.redirect("/")));
 app.use("/", express.static("public"));
 
-const update = (i, callback) => require(`./updater/${updaters[i]}`)
-  .run(() => i >= updaters.length - 1 ? callback() : update(i + 1, callback));
+const update = (i, callback) => updaters[i].run(() => i >= updaters.length - 1 ? callback() : update(i + 1, callback));
